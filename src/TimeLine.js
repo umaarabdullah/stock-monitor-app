4ad43cdd760c282832755682163b5bbe9cbd2f72
umaarabdullah <umaar71@gmail.com>
2023-05-09 08:26:30 +0600

    Update Timeline.js

--- src/TimeLine.js
+++ src/TimeLine.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TimeLine.css'
 
 function TimeLine() {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  }
+
   return (
     <div className="timeline__container">
         <div className="timeline__buttons__container">
-            <div className="timeline__button">LIVE</div>
-            <div className="timeline__button">1D</div>
-            <div className="timeline__button active">1W</div>
-            <div className="timeline__button">3M</div>
-            <div className="timeline__button">1Y</div>
-            <div className="timeline__button">ALL</div>                                                                                
+            {isActive && <div className="timeline__button active" onClick={handleClick}>LIVE</div>}
+            {!isActive && <div className="timeline__button" onClick={handleClick}>LIVE</div>}
+
+            {isActive && <div className="timeline__button active" onClick={handleClick}>1D</div>}
+            {!isActive && <div className="timeline__button" onClick={handleClick}>1D</div>}
+
+            {isActive && <div className="timeline__button active" onClick={handleClick}>1W</div>}
+            {!isActive && <div className="timeline__button" onClick={handleClick}>1W</div>}
+
+            {isActive && <div className="timeline__button active" onClick={handleClick}>3M</div>}
+            {!isActive && <div className="timeline__button" onClick={handleClick}>3M</div>}
+
+            {isActive && <div className="timeline__button active" onClick={handleClick}>1Y</div>}
+            {!isActive && <div className="timeline__button" onClick={handleClick}>1Y</div>}
+
+            {isActive && <div className="timeline__button active" onClick={handleClick}>ALL</div>}
+            {!isActive && <div className="timeline__button" onClick={handleClick}>ALL</div>}                                                                           
         </div>
     </div>
   )
